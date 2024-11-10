e97c1da35cf6cbbe8bcee6d7d9577393752160dd
AlejandroSalmeron09 <21000370@alumnos.utleon.edu.mx>
2024-11-10 15:57:03 -0600

    Add create_app

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ from flask import Flask
 from api import get_items, get_item, add_sale, get_sales
 from flask_cors import CORS 
 
-app = Flask(__name__)
-CORS(app) 
-app.add_url_rule('/api/items', 'get_items', get_items, methods=['GET'])
-app.add_url_rule('/api/items/<int:item_id>', 'get_item', get_item, methods=['GET'])
-app.add_url_rule('/api/addSale', 'add_sale', add_sale, methods=['POST'])
-app.add_url_rule('/api/sales', 'get_sales', get_sales, methods=['GET'])
+def app_create():
+    app = Flask(__name__)
+    CORS(app) 
+    app.add_url_rule('/api/items', 'get_items', get_items, methods=['GET'])
+    app.add_url_rule('/api/items/<int:item_id>', 'get_item', get_item, methods=['GET'])
+    app.add_url_rule('/api/addSale', 'add_sale', add_sale, methods=['POST'])
+    app.add_url_rule('/api/sales', 'get_sales', get_sales, methods=['GET'])
+    return app
 
 if __name__ == '__main__':
+    app = app_create()
     app.run(debug=True, port=5000)
