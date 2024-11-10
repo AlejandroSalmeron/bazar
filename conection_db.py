2c30d3f9c4d5d93d82bd383e258c13e36f4a00ec
AlejandroSalmeron09 <21000370@alumnos.utleon.edu.mx>
2024-11-10 17:38:06 -0600

    ADD NEW CONECTION

--- conection_db.py
+++ conection_db.py
@@ -2,8 +2,8 @@ import mysql.connector
 
 def get_db_connection():
     return mysql.connector.connect(
-        host='examenserver.mysql.database.azure.com',
-        user='examenserver',
-        password='Salmeron123',
-        database='bazar'
+        host='bm5w7hhbnjjcawpyg9qb-mysql.services.clever-cloud.com',
+        user='uiutv6bpdupqiuf2',
+        password='6AANJTlYWgl2yssBQkiA',
+        database='bm5w7hhbnjjcawpyg9qb'
     )
